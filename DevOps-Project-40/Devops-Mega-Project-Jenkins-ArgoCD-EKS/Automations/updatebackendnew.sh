83119e0240a351f358f6ec865a85ddd9e71f4ccd
Nikolaos Zerzelidis <nickzerze@gmail.com>
2026-05-22 10:53:39 +0300

    Update FRONTEND_URL in updatebackendnew.sh

--- DevOps-Project-40/Devops-Mega-Project-Jenkins-ArgoCD-EKS/Automations/updatebackendnew.sh
+++ DevOps-Project-40/Devops-Mega-Project-Jenkins-ArgoCD-EKS/Automations/updatebackendnew.sh
@@ -15,7 +15,7 @@ current_url=$(sed -n "4p" $file_to_find)
 # Update the .env file if the IP address has changed
 if [[ "$current_url" != "FRONTEND_URL=\"http://${ipv4_address}:31000\"" ]]; then
     if [ -f $file_to_find ]; then
-        sed -i -e "s|FRONTEND_URL.*|FRONTEND_URL=\"http://${ipv4_address}:5173\"|g" $file_to_find
+        sed -i -e "s|FRONTEND_URL.*|FRONTEND_URL=\"http://${ipv4_address}:31000\"|g" $file_to_find
     else
         echo "ERROR: File not found."
     fi
